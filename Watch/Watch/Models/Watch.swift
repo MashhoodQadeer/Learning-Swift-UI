6c36665a078975e019eca67969c8f5edd74da931
MashhoodQadeer <mas7qa387@gmail.com>
2021-06-12 04:03:28 +0500

    Updated

---
+++ Watch/Watch/Models/Watch.swift
@@ -0,0 +1,42 @@
+//  Watch.swift
+//  Created by Mashhood Qadeer on 12/06/2021.
+
+import Foundation
+import UIKit
+
+class Watch : Identifiable {
+    
+    var image: UIImage = UIImage()
+    var title: String = ""
+    var price: String = ""
+    var color: String = ""
+    var length: String = ""
+    var chainType: String = ""
+    var chainLength: String = ""
+    var description: String = ""
+    
+    init() {
+        
+    }
+    
+    init(
+        image: UIImage,
+        title: String,
+        price: String,
+        color: String,
+        length: String,
+        chainType: String,
+        chainLength: String,
+        description: String
+        ) {
+        self.image = image
+        self.title = title
+        self.price = price
+        self.color = color
+        self.length = length
+        self.chainType = chainType
+        self.chainLength = chainLength
+        self.description = description
+    }
+    
+}
--- Watch/Watch/Screens/ContentView.swift
+++ Watch/Watch/Screens/ContentView.swift
@@ -7,36 +7,126 @@ import SwiftUI
 //General Data
 var productGategories: [String] = ["ALL", "CLASSIC","GRAND","FORMAL","ORIGINAL"]
 
+var watchesList: [Watch] = [
+    
+    Watch(image: #imageLiteral(resourceName: "watch"), title: "Watch", price: "$110", color: "Black", length: "155mm", chainType: "Leather", chainLength: "155mm", description: "Lorem Ipsum refers to a dummy block of text that is often used in publishing and graphic design to fill gaps in the page before the actual words are put into the finished product. Lorem ipsum resembles Latin but has no real meaning"),
+    Watch(image: #imageLiteral(resourceName: "watch"), title: "Watch", price: "$110", color: "Black", length: "155mm", chainType: "Leather", chainLength: "155mm", description: "Lorem Ipsum refers to a dummy block of text that is often used in publishing and graphic design to fill gaps in the page before the actual words are put into the finished product. Lorem ipsum resembles Latin but has no real meaning"),
+    Watch(image: #imageLiteral(resourceName: "watch"), title: "Watch", price: "$110", color: "Black", length: "155mm", chainType: "Leather", chainLength: "155mm", description: "Lorem Ipsum refers to a dummy block of text that is often used in publishing and graphic design to fill gaps in the page before the actual words are put into the finished product. Lorem ipsum resembles Latin but has no real meaning"),
+    Watch(image: #imageLiteral(resourceName: "watch"), title: "Watch", price: "$110", color: "Black", length: "155mm", chainType: "Leather", chainLength: "155mm", description: "Lorem Ipsum refers to a dummy block of text that is often used in publishing and graphic design to fill gaps in the page before the actual words are put into the finished product. Lorem ipsum resembles Latin but has no real meaning"),
+    Watch(image: #imageLiteral(resourceName: "watch"), title: "Watch", price: "$110", color: "Black", length: "155mm", chainType: "Leather", chainLength: "155mm", description: "Lorem Ipsum refers to a dummy block of text that is often used in publishing and graphic design to fill gaps in the page before the actual words are put into the finished product. Lorem ipsum resembles Latin but has no real meaning")
+]
+
 struct ContentView: View {
     
     @State var categorySelected: String = productGategories[0];
+    @State var selectedWatch: Watch?
     
     func selectedCategory(category : String){
         self.categorySelected = category
     }
     
     var body: some View {
-        VStack{
-            ZStack{
-                HomeScreenBackground()
-                VStack{
-                    HeaderSection()
-                    HeaderSubSection()
-                    CategorySection(selectedCategory: self.selectedCategory(category:), categoriesList: productGategories, selectedCatgory: self.$categorySelected)
+        GeometryReader { meter in
+            VStack{
+                ZStack{
+                    HomeScreenBackground()
+                    VStack{
+                        HeaderSection()
+                        HeaderSubSection()
+                        CategorySection(selectedCategory: self.selectedCategory(category:), categoriesList: productGategories, selectedCatgory: self.$categorySelected)
+                        
+                        ScrollView(.horizontal, showsIndicators : false){
+                                HStack(spacing: 10){
+                                    ForEach( watchesList, id: \.id ) { watch in
+                                    ProductItem(watch: watch)
+                                    }
+                                }.padding(.horizontal, 10)
+                            }
+                            Spacer()
+                    }
                     
-                    ProductItem()
-                    
-                    Spacer()
+                    VStack( alignment: .leading ){
+                        
+                        HeaderSection(foregroundColors: .white)
+                        HeaderSubSection(foregroundColors: .white)
+                        Spacer().frame(height : 30)
+                        //Body Content
+                        ZStack{
+                            //For the Title
+                            VStack(alignment :  .leading ){
+                                HStack{
+                                    VStack(alignment: .leading){
+                                        Text("Type")
+                                            .font(.headline)
+                                            .foregroundColor(.white)
+                                        Text(self.selectedWatch?.title ?? "N/A")
+                                            .font(.caption)
+                                            .foregroundColor(.white).padding(.horizontal,5)
+                                    }
+                                    Spacer()
+                                }
+                                Spacer().frame(height: 15)
+                                VStack(alignment: .leading){
+                                    Text("Color")
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                    Text(self.selectedWatch?.color ?? "N/A")
+                                        .font(.caption)
+                                        .foregroundColor(.white).padding(.horizontal,5)
+                                }
+                                Spacer().frame(height: 15)
+                                VStack(alignment: .leading){
+                                    Text("Price")
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                    Text(self.selectedWatch?.price ?? "N/A")
+                                        .font(.caption)
+                                        .foregroundColor(.white).padding(.horizontal,5)
+                                }
+                                Spacer()
+                            }.frame( width: meter.size.width)
+                            ZStack{
+                                
+                                VStack{
+                                    Spacer()
+                                    VStack(alignment: .leading){
+                                        Text(self.selectedWatch?.title ?? "N/A")
+                                        Text(self.selectedWatch?.description ?? "N/A")
+                                    }.frame( width: meter.size.width).offset(y:35)
+                                    .frame(height: meter.size.height * 0.40)
+                                    .background(Color.white)
+                                    .cornerRadius(30)
+                                }
+                                
+                                VStack{
+                                    HStack(){
+                                        Spacer()
+                                        Image(uiImage: ((self.selectedWatch?.image) ?? UIImage(named: "watch")) ?? UIImage() ).resizable().frame(width: meter.size.width * 0.50, height: meter.size.height * 0.45)
+                                            .rotationEffect(.init(degrees: -15))
+                                    }
+                                        
+                                    Spacer().frame(height: meter.size.height * 0.15)
+                                }
+                                
+                                
+                            }.frame( width: meter.size.width).offset(y:35)
+                        }
+                        
+                    }.frame(height: .infinity)
+                    .background(VStack{
+                        Color.black.ignoresSafeArea()
+                    })
                 }
             }
         }
+       
     }
     
 }
 
-
 struct ProductItem : View {
     @State var widthView: CGFloat = 200
+    @State var watch: Watch
     var body: some View {
         ZStack{
             VStack(alignment: .leading, spacing: 0, content: {
@@ -51,11 +141,12 @@ struct ProductItem : View {
                         .font(.title)
                     Spacer().frame(height:10)
                 })
-            })
-            Button(action: { }, label: {
+            }).overlay(Button(action: { }, label: {
                 Image(systemName: "plus.circle.fill")
                     .foregroundColor(.orange).font(.title)
-            }).frame( alignment: .bottomTrailing)
+            }).frame(maxWidth: .infinity, maxHeight: .infinity,alignment: .bottomTrailing))
+            .padding(.vertical)
+            
         }.frame(width: self.widthView )
         .background(
             VStack{
@@ -70,17 +161,18 @@ struct ProductItem : View {
 }
 
 struct HeaderSection: View {
+    var foregroundColors: Color = .black
     var body: some View {
         HStack{
             Button(action: {}, label: {
-                Image(systemName: "line.horizontal.3.decrease.circle").foregroundColor(.black)
-                    .foregroundColor(.black)
+                Image(systemName: "line.horizontal.3.decrease.circle").foregroundColor(self.foregroundColors)
+                    .foregroundColor(self.foregroundColors)
                         .frame(width: 30, height: 30)
                         .scaledToFit()
             })
             Spacer()
             Button(action: {}, label: {
-                Image(systemName: "cart.circle").foregroundColor(.black)
+                Image(systemName: "cart.circle").foregroundColor(self.foregroundColors)
                     .frame(width: 30, height: 30)
                     .scaledToFit()
             })
@@ -97,22 +189,23 @@ struct HomeScreenBackground : View {
 }
 
 struct HeaderSubSection : View {
+      var foregroundColors: Color = .black
       var body : some View {
         VStack( spacing : 0 ){
             HStack{
                 VStack(alignment: .leading){
                     Text("Welcome!")
                         .font(.subheadline)
-                        .foregroundColor(.black)
+                        .foregroundColor(self.foregroundColors)
                     Text("Discovery")
                         .font(.title)
                         .fontWeight(.heavy)
-                        .foregroundColor(.black)
+                        .foregroundColor(self.foregroundColors)
                 }.padding(.horizontal, 5)
                 Spacer()
                 Button(action: {}, label: {
-                    Image(systemName: "magnifyingglass").foregroundColor(.black)
-                        .foregroundColor(.black)
+                    Image(systemName: "magnifyingglass").foregroundColor(self.foregroundColors)
+                        .foregroundColor(self.foregroundColors)
                             .frame(width: 30, height: 30)
                             .scaledToFit()
                 })
